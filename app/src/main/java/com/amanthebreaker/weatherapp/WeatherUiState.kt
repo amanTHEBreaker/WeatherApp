4ed33c6e2d2adf1c5f2c1cfd05a064e6acd35a70
abaja <rj533698@gmail.com>
2025-08-16 15:03:08 +0530

    The scratch code for the viewmodel with recommended architecture

--- app/src/main/java/com/amanthebreaker/weatherapp/WeatherUiState.kt
+++ app/src/main/java/com/amanthebreaker/weatherapp/WeatherUiState.kt
@@ -1,7 +1,9 @@
 package com.amanthebreaker.weatherapp
 
-data class WeatherUiState (
-    val currentCity : String = "",
-    val temp : String = "",
-    val humidity : String = ""
+data class WeatherUiState(
+    val cityName: String = "",
+    val temp: String = "",
+    val humidity: String = "",
+    val isLoading: Boolean = false,
+    val error: String? = null
 )
